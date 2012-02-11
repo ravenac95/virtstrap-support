380c4c2f01b224684b1f385c625e1fbdfb397f84
Reuven V. Gonzales <reuven@tobetter.us>
2012-02-10 17:19:36 -0800

    Virtstrap support files

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.3.0-alpha'
+
+setup(name='virtstrap-support',
+    version=version,
+    description="",
+    long_description="""\
+""",
+    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='',
+    author='Reuven V. Gonzales',
+    author_email='reuven@tobetter.us',
+    url='',
+    license='MIT',
+    packages=['virtstrap_support'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+    entry_points='',
+)
